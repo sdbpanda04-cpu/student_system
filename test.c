4131d12398875b618d6828d2639fb8d455e6b921
sdbpanda04-cpu <sdbpanda04@gmail.com>
2025-11-12 14:04:51 +0530

    Add reverse-string program in test.c

---
+++ test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char s[1024];
+
+	/* Read a line from stdin (up to 1023 chars). */
+	if (fgets(s, sizeof(s), stdin) == NULL) {
+		return 0;
+	}
+
+	/* Remove trailing newline if present. */
+	size_t len = strlen(s);
+	if (len > 0 && s[len - 1] == '\n') {
+		s[--len] = '\0';
+	}
+
+	/* Reverse the string in place. */
+	for (size_t i = 0; i < len / 2; ++i) {
+		char tmp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = tmp;
+	}
+
+	/* Print the reversed string. */
+	printf("%s\n", s);
+	return 0;
+}
